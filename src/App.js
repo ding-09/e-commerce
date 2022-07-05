b184678e8263694bd8698615410b393a6c8d95cc
Tiffany <yang.ding@hotmail.com>
2022-07-05 13:12:55 -0700

    created blank home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/header';
+import HomePage from './pages/home';
 
 const App = () => {
   return (
     <>
       <Header />
+      <HomePage />
     </>
   );
 };
